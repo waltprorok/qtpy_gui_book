26996424f19e24ed01a1ae26cce96d98d4d3527d
Walter Prorok <waltprorok.com>
2020-04-13 18:40:50 -0400

    updated gui

--- Chapter1/demoLineEdit.py
+++ Chapter1/demoLineEdit.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'demoLineEdit.ui'
 #
-# Created by: PyQt5 UI code generator 5.14.1
+# Created by: PyQt5 UI code generator 5.14.2
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -13,19 +13,19 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
+        Dialog.resize(425, 300)
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(30, 60, 81, 16))
+        self.label.setGeometry(QtCore.QRect(30, 60, 121, 16))
         self.label.setObjectName("label")
         self.labelResponse = QtWidgets.QLabel(Dialog)
         self.labelResponse.setGeometry(QtCore.QRect(30, 110, 331, 16))
         self.labelResponse.setText("")
         self.labelResponse.setObjectName("labelResponse")
         self.lineEditName = QtWidgets.QLineEdit(Dialog)
-        self.lineEditName.setGeometry(QtCore.QRect(130, 60, 231, 20))
+        self.lineEditName.setGeometry(QtCore.QRect(150, 60, 231, 20))
         self.lineEditName.setObjectName("lineEditName")
         self.ButtonClickMe = QtWidgets.QPushButton(Dialog)
-        self.ButtonClickMe.setGeometry(QtCore.QRect(130, 210, 91, 23))
+        self.ButtonClickMe.setGeometry(QtCore.QRect(290, 210, 91, 23))
         self.ButtonClickMe.setCheckable(False)
         self.ButtonClickMe.setChecked(False)
         self.ButtonClickMe.setObjectName("ButtonClickMe")
